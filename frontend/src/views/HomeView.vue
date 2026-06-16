6a5d17a3da2a38aaf428603a90433d403b47508b
Windpicker-owo <3431391539@qq.com>
2026-06-16 14:47:56 +0800

    feat: 添加 ref 导入以支持响应式数据管理

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useHomeStore } from '@/stores/home'
 import { useTaxonomyStore } from '@/stores/taxonomy'
